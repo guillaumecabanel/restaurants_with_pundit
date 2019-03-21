62ecc0328a12507f2f7a0c0977a8fb39a4e952a3
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2019-03-21 09:40:28 +0100

    wip lecture

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,22 +2,26 @@ class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
-    @restaurants = Restaurant.all
+    @restaurants = policy_scope(Restaurant)
   end
 
   def show
+    authorize @restaurant
   end
 
   def new
     @restaurant = Restaurant.new
+    authorize @restaurant
   end
 
   def edit
+    authorize @restaurant
   end
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.user = current_user
+    authorize @restaurant
 
     if @restaurant.save
       redirect_to @restaurant
@@ -27,6 +31,7 @@ class RestaurantsController < ApplicationController
   end
 
   def update
+    authorize @restaurant
     if @restaurant.update(restaurant_params)
       redirect_to @restaurant
     else
@@ -37,6 +42,7 @@ class RestaurantsController < ApplicationController
   # DELETE /restaurants/1
   # DELETE /restaurants/1.json
   def destroy
+    authorize @restaurant
     @restaurant.destroy
     redirect_to restaurants_path
   end
---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,40 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+
+      # dans le cas d'un SAAS par exemple
+      # La liste des restaurants ne contient que
+      # les restaurants de l'utilisateur
+      # Restaurant.where(....)
+      # scope.where(user: user)
+    end
+  end
+
+  def create?
+    true # anyone can create a restaurant
+  end
+
+  def show?
+    true # anyone can view a restaurant
+  end
+
+  def update?
+    # only onwer can edit a restaurant
+    # user <=> current_user
+    # record <=> l'argument passé à `authorize` dans le controller
+    # record <=> @restaurant
+
+    restaurant_owner?
+  end
+
+  def destroy?
+    restaurant_owner?
+  end
+
+  private
+
+  def restaurant_owner?
+    record.user == user
+  end
+end
